ff9f606a4d353d2222c896a2ae9d689b538539b4
Jared Jennings <jjennings@fastmail.fm>
2015-08-13 09:20:44 -0500

    use strings for file resource mode parameter: Puppet 4 cares

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -30,7 +30,7 @@ class aide::redhat {
 # Configure AIDE to create and monitor a baseline of database ``software
 # libraries, related applications and configuration files.''
     file { "/etc/aide.conf":
-        owner => root, group => 0, mode => 0600,
+        owner => root, group => 0, mode => '0600',
         source => "puppet:///modules/aide/aide.conf",
     }
 
@@ -62,14 +62,14 @@ class aide::redhat {
 # \implements{unixsrg}{GEN003100,GEN003120,GEN003140}%
 # Use mode \verb!0700! for the daily log rotation script, as required.
     file { "/etc/cron.daily/logrotate":
-        owner => root, group => 0, mode => 0700,
+        owner => root, group => 0, mode => '0700',
         source => "puppet:///modules/aide/logrotate",
     }
 
 # Install the baseline backup script for use by administrators. See
 # \S\ref{backup_baseline_usage}.
     file { "/usr/sbin/backup_baseline.sh":
-        owner => root, group => 0, mode => 0755,
+        owner => root, group => 0, mode => '0755',
         source => "puppet:///modules/aide/backup_baseline.sh",
     }
 }
